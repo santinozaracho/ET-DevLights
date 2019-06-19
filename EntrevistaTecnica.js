5d0cc4a11ec2cfc7559c36ad07b7677cd0c7fdcd
Carlos Santino Zaracho Simonetto <40505201+santinozaracho@users.noreply.github.com>
2019-06-19 13:59:23 -0300

    Se encontro una funcion mas optima (no propia) para obtener combinaciones

--- EntrevistaTecnica.js
+++ EntrevistaTecnica.js
@@ -23,6 +23,7 @@ const StrFunctionCalc = (stringIn) => {
     if ( stringIn.length < 10**5 ) {
         let string = stringIn.trim().toLowerCase()
         let results = generateAllsCom(string).map(subStr => searchAndCount(string,subStr) * subStr.length)
+        // let results = combinationOfLetters(string).map(subStr => searchAndCount(string,subStr) * subStr.length)
         // return Math.max.apply(null,results)   
         return getMaxValFromArr(results)
     }else {
@@ -60,4 +61,76 @@ const searchAndCount = (string,subStr) => {
         count++,index=string.indexOf(subStr,index+1) );
     // console.log(subStr+' Qty:'+count);
     return count
+}
+
+
+/* this function for get all combinations of string was finded in StackOverflow, an this is the most optimized function*/
+function process(w, word) {
+    let temp = [];
+    for (let j = 1; j < word.length; j++) {
+        temp.push(w + word[j].charAt(word[j].length - 1));
+    }
+    return temp;
+}
+
+function combinationOfLetters(word) {
+    let arr = [],
+        copy = [
+            []
+        ],
+        copyCondition = true,
+        save = word.split(''),
+        copyInx = 0;
+
+    function loopFunc(prword) {
+
+        if (copy[0].includes(word)) {
+            if (copyInx === 0) arr.unshift(save);
+            //recording the index of the last element in the array
+            //we'll use it later, when creating a new 'copy' array;
+            copyInx = arr.length;
+            //to be able to create a new 'copy' array
+            //(with the next loopFunc call) we set it to true;
+            copyCondition = true;
+            return save.slice(1);
+        }
+        //explicit block    
+        {
+            let w;
+
+            while (prword.length > 1) {
+                w = prword[0];
+                prword = process(w, prword);
+                arr.push(prword);
+            }
+            //end of explicit block
+        }
+        if (copyCondition) {
+            copyCondition = false;
+            copy = [];
+            //we don't want to process any old values again
+            //so we are using copyInx to copy only new values (into the copy array)
+            //that need to be processed.
+            for (let val of arr.slice(copyInx)) {
+                let copyVal = [];
+                [].push.apply(copyVal, val);
+                copy.push(copyVal);
+            }
+        }
+
+        if (copy[0].length === 1 && !copy[0].includes(word)) {
+            copy.shift();
+        }
+        if (copy[0].length > 1 && !copy[0].includes(word)) {
+            copy[0].shift();
+        }
+        return loopFunc(copy[0]);
+    }
+    //creating all combs of a string
+    do {
+        save = loopFunc(save);
+        word = word.slice(1);
+    } while (save.length > 1);
+    //when arr is created return it
+    return arr.reduce((a, b) => a.concat(b));
 }
